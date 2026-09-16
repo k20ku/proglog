bfa24bc2283dc7351a40a1060e16e74f76d5447f
k20ku <heizhaokuan@gmail.com>
2026-09-16 21:20:25 +0900

    chore(config): remove unnecessary comments in TLS config

--- internal/config/tls_config.go
+++ internal/config/tls_config.go
@@ -3,8 +3,8 @@ package config
 type TLSConfig struct {
 	CertDir       string `env:"CERT_DIR,expand" envDefault:"${HOME}/.proglog/cert"`
 	CACertFile    string `env:"CA_CERT,expand" envDefault:"${CERT_DIR}/ca.pem"`
-	CertFile      string `env:"SERVER_CERT,expand" envDefault:"${CERT_DIR}/server.crt"`
-	KeyFile       string `env:"SERVER_KEY,expand" envDefault:"${CERT_DIR}/server.key"`
+	CertFile      string
+	KeyFile       string
 	Server        bool
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost"`
 }
